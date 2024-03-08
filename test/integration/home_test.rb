089a0a07cc233c1da52a5c20259e9323e9f78b29
Jogo Hiroki <jh147896325jh@gmail.com>
2024-03-08 19:58:37 +0900

    ホーム画面を作った。テストも通過した。

---
+++ test/integration/home_test.rb
@@ -0,0 +1,13 @@
+require "test_helper"
+
+class HomeTest < ActionDispatch::IntegrationTest
+  test "should get home" do
+    get root_path
+    assert_response :success
+  end
+
+  test "should get home 2" do
+    get home_path
+    assert_response :success
+  end
+end
