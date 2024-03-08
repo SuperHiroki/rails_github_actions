1ef7e20d76cdd26ca6a150cedd00c59f1ff8b95e
Jogo Hiroki <jh147896325jh@gmail.com>
2024-03-08 20:02:00 +0900

    テストを少し修正した。

--- test/integration/home_test.rb
+++ test/integration/home_test.rb
@@ -4,10 +4,12 @@ class HomeTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get root_path
     assert_response :success
+    assert_select "p", "ここはホームです。"
   end
 
   test "should get home 2" do
     get home_path
     assert_response :success
+    assert_select "p", "ここはホームです。"
   end
 end
